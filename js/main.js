62a25e18b0f04f21713b55dcbff523868dafa766
Natalia <n.platova@hotmail.com>
2020-03-27 11:20:24 +0700

    add js to feedback button and add-to-basket button

--- js/main.js
+++ js/main.js
@@ -10,4 +10,16 @@ $(document).ready(function(){
         prevArrow: '<button type="button" class="slider__arrow slider__arrow_prev"></button>',
         nextArrow: '<button type="button" class="slider__arrow slider__arrow_next"></button>',
     });
+
+    $('.feedback-button').on('click', function() {
+        $('.feedback__overlay').css('display', 'flex');
+    });
+
+    $('.feedback__close').on('click', function () {
+        $('.feedback__overlay').css('display', 'none');
+    });
+    
+    $('.add-to-basket').on('click', function () {
+        alert('Товар добавлен в корзину');
+    });
 });
